671a3d8dc6ae04d919d3e08d017339df33b98527
braziel <braziel@discente.ufg.br>
2024-09-25 15:26:08 -0300

    changes

--- Compiler/BoolCompiler.java
+++ Compiler/BoolCompiler.java
@@ -3,27 +3,40 @@ package Compiler;
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class BoolCompiler {
 
     public static void main(String[] args){
-        if(args.length == 2) {
-            String boolFileName = args[0];
-            String compiledFileName = args[1];
-        } else {
+        if(args.length != 2) {
             System.out.println("Input and Output files need to be informed when running the program");
             return;
+            
         }
-        File filePath = new File("Data/examplefile.bool");
+        String boolFileName = args[0];
+        String compiledFileName = args[1];
 
-        try(BufferedReader br = new BufferedReader(new FileReader(filePath.getPath()))){
-            String line = br.readLine();
+        File filePath = new File(boolFileName);
+        File compiledFilePath = new File(compiledFileName);
+
+        // Reading the bool file
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath.getPath()))){ 
+            String line = br.readLine();  
 
             while(line != null){
                 System.out.println(line);
                 line = br.readLine();
             }
+
+        } catch (IOException error){
+            System.out.println("Error: " + error.getMessage());
+        }
+
+        // FAZER A PARTE DE LEITURA
+        // Writing the bool compiled file
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(compiledFilePath.getPath()))){
 
         } catch (IOException error){
             System.out.println("Error: " + error.getMessage());
---
+++ Utils/io.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class io {
+    public static int print(int value){
+        System.out.println(value);
+        return 0;
+    }
+}
