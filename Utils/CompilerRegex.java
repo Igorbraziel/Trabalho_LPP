c059d664bbc99738342ba21027e2d4dbde8a7abb
braziel <braziel@discente.ufg.br>
2024-09-25 16:51:33 -0300

    Update CompilerRegex.java

--- Utils/CompilerRegex.java
+++ Utils/CompilerRegex.java
@@ -7,5 +7,4 @@ public class CompilerRegex {
     public Pattern classPatt = Pattern.compile("^\\s*class\\s+\\w+$");
     public Pattern varsPatt = Pattern.compile("^(\\s*vars\\s+\\w+(\\s*,\\s*\\w+)*)$");
     public Pattern methodPatt = Pattern.compile("^(\\s*method\\s+\\w+\\((\\w*(\\s*,\\s*\\w+\\s*)*)\\)\\s*)$");
-
 }
