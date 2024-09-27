e469ef2b6837b8adc7790db85fd27ab712bed52b
braziel <braziel@discente.ufg.br>
2024-09-27 14:13:24 -0300

    Update AtribuicaoComOperador.java

--- src/Compiler/AtribuicaoComOperador.java
+++ src/Compiler/AtribuicaoComOperador.java
@@ -5,21 +5,39 @@ import java.util.regex.Pattern;
 
 public class AtribuicaoComOperador {
 
-    public static String atribuicao(String linhaOriginal){
+    public static String atribuicaoComOperador(String linhaOriginal){
         String falha = "FALHA";
         String linhaCompilada = falha;
 
-        Pattern pattern = Pattern.compile("^(\\s*)(\\w+)\\s+=\\s+(\\w+)\\s+(\\+)\\s+(\\w+)\\s*$");
+        Pattern pattern = Pattern.compile("^(\\s*)(\\w+)\\s+=\\s+(\\w+)\\s+([+\\-*/])\\s+(\\w+)\\s*$");
         Matcher matcher = pattern.matcher(linhaOriginal);
-        if(matcher.find()){ // c = a + b
-            linhaCompilada = "\n" + matcher.group(1) + "load " + matcher.group(3) + "\n" + matcher.group(1) + "load " + matcher.group(5)
-                    + "\n" + matcher.group(1) + "add" + "\n" + matcher.group(1) + "store " + matcher.group(2);
-        } else { // c = a - b
-            pattern = Pattern.compile("^(\\s*)(\\w+)\\s+=\\s+(\\w+)\\s+(-)\\s+(\\w+)\\s*$");
-            matcher = pattern.matcher(linhaOriginal);
-            if(matcher.find()){
+        if(matcher.find()){ // c = a + b // acha qualquer operador + - * /
+            if(matcher.group(4).equals("+")) { // c = a + b
+                linhaCompilada = "\n" + matcher.group(1) + "load " + matcher.group(3) + "\n" + matcher.group(1) + "load " + matcher.group(5)
+                        + "\n" + matcher.group(1) + "add" + "\n" + matcher.group(1) + "store " + matcher.group(2);
+            } else if (matcher.group(4).equals("-")){
                 linhaCompilada = "\n" + matcher.group(1) + "load " + matcher.group(3) + "\n" + matcher.group(1) + "load " + matcher.group(5)
                         + "\n" + matcher.group(1) + "sub" + "\n" + matcher.group(1) + "store " + matcher.group(2);
+            } else if(matcher.group(4).equals("*")){
+                linhaCompilada = "\n" + matcher.group(1) + "load " + matcher.group(3) + "\n" + matcher.group(1) + "load " + matcher.group(5)
+                        + "\n" + matcher.group(1) + "mul" + "\n" + matcher.group(1) + "store " + matcher.group(2);
+            } else if(matcher.group(4).equals("/")){
+                linhaCompilada = "\n" + matcher.group(1) + "load " + matcher.group(3) + "\n" + matcher.group(1) + "load " + matcher.group(5)
+                        + "\n" + matcher.group(1) + "div" + "\n" + matcher.group(1) + "store " + matcher.group(2);
+            }
+        } else { // name.name = a + b //atribuição com operador a um name.name
+            pattern = Pattern.compile("^(\\s*)(\\w+)\\.(\\w+)\\s+=\\s+(\\w+)\\s+([+\\-*/])\\s+(\\w+)\\s*$");
+            matcher = pattern.matcher(linhaOriginal);
+            if(matcher.find()){
+                if(matcher.group(5).equals("+")) {
+                    // falta desenvolver
+                } else if(matcher.group(5).equals("-")){
+                    // falta desenvolver
+                } else if(matcher.group(5).equals("*")){
+                    // falta desenvolver
+                } else if(matcher.group(5).equals("/")){
+                    // falta desenvolver
+                }
             }
         }
 
