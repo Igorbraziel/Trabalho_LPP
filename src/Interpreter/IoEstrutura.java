1bb9fb71176bdbd7f948c1bd67604441b2454398
braziel <braziel@discente.ufg.br>
2024-10-29 10:06:16 -0300

    Update IoEstrutura.java

--- src/Interpreter/IoEstrutura.java
+++ src/Interpreter/IoEstrutura.java
@@ -2,7 +2,7 @@ package Interpreter;
 
 import java.util.List;
 
-public class IoEstrutura extends EstruturaObjeto{
+public class IoEstrutura extends EstruturaObjeto {
     public void instrucaoPrint(List<Var> pilha){
         System.out.println("Resultado do print: " + pilha.getFirst().getValor());
     }
